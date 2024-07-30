63d7a2ea137905b995d3c9c3fb5f513180837f23
Rycherd Dheurymy da Silva <dheurymy@outlook.com>
2024-07-30 05:16:24 -0300

    add title em sobre

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -19,122 +19,109 @@ import linkedin from '../../assets/images/icons/iconeLinkedin.svg'
 export default function Sobre() {
 
    return (
-      <div className={styleSobre.about}>
-         <div className={styleSobre.card}>
-            <div className={styleSobre.painel}>
-            <img src={foto} alt="" className={styleSobre.image} />
 
-            <div className={styleSobre.links}>
-               <a href="https://www.instagram.com/dheurymy" >
-                  <img src={insta} alt="" title="Instagram do Dheurymy"/>
-               </a>
-
-               <a href="https://github.com/dheurymy">
-                  <img src={git} alt="" title="Github do Dheurymy"/>
-               </a>
-
-               <a href="https://www.linkedin.com/in/dheurymy/">
-                  <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
-               </a>
+      <div className={styleSobre.page}>
+         <h1  className={styleSobre.pageTitle}> Conheça os desenvolvedores do <strong>Explore o Maranhão</strong>: </h1>
+         <div className={styleSobre.about}>
+         
+            <div className={styleSobre.card}>
+               <div className={styleSobre.painel}>
+               <img src={foto} alt="" className={styleSobre.image} />
+               <div className={styleSobre.links}>
+                  <a href="https://www.instagram.com/dheurymy" >
+                     <img src={insta} alt="" title="Instagram do Dheurymy"/>
+                  </a>
+                  <a href="https://github.com/dheurymy">
+                     <img src={git} alt="" title="Github do Dheurymy"/>
+                  </a>
+                  <a href="https://www.linkedin.com/in/dheurymy/">
+                     <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
+                  </a>
+               </div>
+               </div>
+               <h1 className={styleSobre.title}>
+                  Rycherd Dheurymy
+               </h1>
+               <p className={styleSobre.legend}>
+                  Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
+                  Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
+               </p>
+               <div className={styleSobre.badges}>
+                  <img src={badgeHTML} alt="" />
+                  <img src={badgeCSS} alt="" />
+                  <img src={badgeJS} alt="" />
+                  <img src={badgeCPP} alt="" />
+                  <img src={badgeNode} alt="" />
+                  <img src={badgePython} alt="" />
+         
+               </div>
+            </div>
+            <div className={styleSobre.card}>
+               <div className={styleSobre.painel}>
+               <img src={foto} alt="" className={styleSobre.image} />
+               <div className={styleSobre.links}>
+                  <a href="https://www.instagram.com/dheurymy" >
+                     <img src={insta} alt="" title="Instagram do Dheurymy"/>
+                  </a>
+                  <a href="https://github.com/dheurymy">
+                     <img src={git} alt="" title="Github do Dheurymy"/>
+                  </a>
+                  <a href="https://www.linkedin.com/in/dheurymy/">
+                     <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
+                  </a>
+               </div>
+               </div>
+               <h1 className={styleSobre.title}>
+                  Rycherd Dheurymy
+               </h1>
+               <p className={styleSobre.legend}>
+                  Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
+                  Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
+               </p>
+               <div className={styleSobre.badges}>
+                  <img src={badgeHTML} alt="" />
+                  <img src={badgeCSS} alt="" />
+                  <img src={badgeJS} alt="" />
+                  <img src={badgeCPP} alt="" />
+                  <img src={badgeNode} alt="" />
+                  <img src={badgePython} alt="" />
+         
+               </div>
+            </div>
+            <div className={styleSobre.card}>
+               <div className={styleSobre.painel}>
+               <img src={foto} alt="" className={styleSobre.image} />
+               <div className={styleSobre.links}>
+                  <a href="https://www.instagram.com/dheurymy" >
+                     <img src={insta} alt="" title="Instagram do Dheurymy"/>
+                  </a>
+                  <a href="https://github.com/dheurymy">
+                     <img src={git} alt="" title="Github do Dheurymy"/>
+                  </a>
+                  <a href="https://www.linkedin.com/in/dheurymy/">
+                     <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
+                  </a>
+               </div>
+               </div>
+               <h1 className={styleSobre.title}>
+                  Rycherd Dheurymy
+               </h1>
+               <p className={styleSobre.legend}>
+                  Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
+                  Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
+               </p>
+               <div className={styleSobre.badges}>
+                  <img src={badgeHTML} alt="" />
+                  <img src={badgeCSS} alt="" />
+                  <img src={badgeJS} alt="" />
+                  <img src={badgeCPP} alt="" />
+                  <img src={badgeNode} alt="" />
+                  <img src={badgePython} alt="" />
+         
+               </div>
             </div>
             </div>
-            <h1 className={styleSobre.title}>
-               Rycherd Dheurymy
-            </h1>
-            <p className={styleSobre.legend}>
-               Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
-               Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
-            </p>
-            <div className={styleSobre.badges}>
-               <img src={badgeHTML} alt="" />
-               <img src={badgeCSS} alt="" />
-               <img src={badgeJS} alt="" />
-               <img src={badgeCPP} alt="" />
-               <img src={badgeNode} alt="" />
-               <img src={badgePython} alt="" />
-               
-
-            </div>
-
-         </div>
-
-         <div className={styleSobre.card}>
-            <div className={styleSobre.painel}>
-            <img src={foto} alt="" className={styleSobre.image} />
-
-            <div className={styleSobre.links}>
-               <a href="https://www.instagram.com/dheurymy" >
-                  <img src={insta} alt="" title="Instagram do Dheurymy"/>
-               </a>
-
-               <a href="https://github.com/dheurymy">
-                  <img src={git} alt="" title="Github do Dheurymy"/>
-               </a>
-
-               <a href="https://www.linkedin.com/in/dheurymy/">
-                  <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
-               </a>
-            </div>
-            </div>
-            <h1 className={styleSobre.title}>
-               Rycherd Dheurymy
-            </h1>
-            <p className={styleSobre.legend}>
-               Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
-               Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
-            </p>
-            <div className={styleSobre.badges}>
-               <img src={badgeHTML} alt="" />
-               <img src={badgeCSS} alt="" />
-               <img src={badgeJS} alt="" />
-               <img src={badgeCPP} alt="" />
-               <img src={badgeNode} alt="" />
-               <img src={badgePython} alt="" />
-               
-
-            </div>
-
-         </div>
-
-         <div className={styleSobre.card}>
-            <div className={styleSobre.painel}>
-            <img src={foto} alt="" className={styleSobre.image} />
-
-            <div className={styleSobre.links}>
-               <a href="https://www.instagram.com/dheurymy" >
-                  <img src={insta} alt="" title="Instagram do Dheurymy"/>
-               </a>
-
-               <a href="https://github.com/dheurymy">
-                  <img src={git} alt="" title="Github do Dheurymy"/>
-               </a>
-
-               <a href="https://www.linkedin.com/in/dheurymy/">
-                  <img src={linkedin} alt=""  title="Linkedin do Dheurymy" />
-               </a>
-            </div>
-            </div>
-            <h1 className={styleSobre.title}>
-               Rycherd Dheurymy
-            </h1>
-            <p className={styleSobre.legend}>
-               Olá, pessoal, sou o Dheurymy! Um engenheiro mecânico que se descobriu no mundo da programação, pai de gatos, fã de Drag Race, Naruto e One Piece, membro orgulhoso da comunidade LGBT+ e amante de viagens e domingos de sol. <br /><br />
-               Aluno de Análise e Desenvolvimento de Sistemas pela Faculdade Laboro e Engenheiro Mecânico formado pelo Instituto Federal do Maranhão. Participante, na trilha de Programação Front-End, do programa Trilhas Inova do Governo do Maranhão com SECTI, FAPEMA e Alura.
-            </p>
-            <div className={styleSobre.badges}>
-               <img src={badgeHTML} alt="" />
-               <img src={badgeCSS} alt="" />
-               <img src={badgeJS} alt="" />
-               <img src={badgeCPP} alt="" />
-               <img src={badgeNode} alt="" />
-               <img src={badgePython} alt="" />
-               
-
-            </div>
-
-         </div>
-
-         </div>
-      
+         
+      </div>
    )
 }
